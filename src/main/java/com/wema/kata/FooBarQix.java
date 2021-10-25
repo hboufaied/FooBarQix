3d24384ab09571ab01f0f73ba0ff9fac0b449ca6
Hamda <hboufaied@wemanity.com>
2021-10-25 16:02:30 +0200

    should return 1*1 when number is 101 : test ok

--- src/main/java/com/wema/kata/FooBarQix.java
+++ src/main/java/com/wema/kata/FooBarQix.java
@@ -9,37 +9,41 @@ public class FooBarQix {
 	private static final String FOO = "Foo";
 	private static final String BAR = "Bar";
 	private static final String QIX = "Qix";
+	private static final String STAR = "*";
 
 	public String compute(String numberToCompute) {
 		StringBuilder result = new StringBuilder();
 
-		Rule ruleForNumberThree = new Rule(3, FOO);
-		Rule ruleForNumberFive = new Rule(5, BAR);
-		Rule ruleForNumberSeven = new Rule(7, QIX);
-		Rule[] rules = {ruleForNumberThree, ruleForNumberFive, ruleForNumberSeven};
-		
-		result.append(computeDevidedNumber(numberToCompute, rules));
+		Rule ruleForNumberDevisibleByThree = new Rule(3, FOO);
+		Rule ruleForNumberDivisibleByFive = new Rule(5, BAR);
+		Rule ruleForNumberDivibleBySeven = new Rule(7, QIX);
 
-		char[] charArrayOfNumber = String.valueOf(numberToCompute).toCharArray();
-		for (char caracter : charArrayOfNumber) {
-			result.append(computeContainNumber(caracter, rules));
-		}
+		Rule[] rulesForReplacement = { ruleForNumberDevisibleByThree, ruleForNumberDivisibleByFive,
+				ruleForNumberDivibleBySeven };
 
-		// if number is not devised by 3 or not contain 3, then we return the same
-		// number
+		result.append(computeDevidedNumber(numberToCompute, rulesForReplacement));
+
+		result.append(computeContainNumber(numberToCompute, rulesForReplacement));
+
+
 		if (result.length() == 0) {
 			return numberToCompute;
 		}
 
-		return result.toString();
+		return result.toString().replace("0", STAR);
 	}
 
-	private String computeContainNumber(char caracter, Rule[] rules) {
+	private String computeContainNumber(String numberToCompute, Rule[] rules) {
 		StringBuilder containNumberResult = new StringBuilder();
 
-		Arrays.stream(rules).forEach(rule -> {
+		numberToCompute.chars().mapToObj(c -> (char)c).forEach(caracter -> {
+			for(Rule rule : rules) {
 			if (Character.getNumericValue(caracter) == rule.getNumber()) {
 				containNumberResult.append(rule.getReplacement());
+			} else if (Character.getNumericValue(caracter) == 0) {
+				containNumberResult.append("0");
+				break;
+			}
 			}
 		});
 		return containNumberResult.toString();
@@ -48,7 +52,7 @@ public class FooBarQix {
 	private String computeDevidedNumber(String numberToCompute, Rule[] rules) {
 		StringBuilder devideNumberResult = new StringBuilder();
 		int number = Integer.parseInt(numberToCompute);
-		
+
 		Arrays.stream(rules).forEach(rule -> {
 			if (number % rule.getNumber() == 0) {
 				devideNumberResult.append(rule.getReplacement());
