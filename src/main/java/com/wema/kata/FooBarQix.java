9ea7dd5d5adb49b8ba7ea327b9f6562157e45b8d
Hamda <hboufaied@wemanity.com>
2021-10-25 10:53:01 +0200

    should return FooFoo when number is 3 : test ok

--- src/main/java/com/wema/kata/FooBarQix.java
+++ src/main/java/com/wema/kata/FooBarQix.java
@@ -3,7 +3,20 @@ package com.wema.kata;
 public class FooBarQix {
 
 	public String compute(int number) {
-		return String.valueOf(number);
+		StringBuilder sb = new StringBuilder();
+		if (number % 3 == 0) {
+			sb.append("Foo");
+		}
+		char[] charArrayOfNumber = String.valueOf(number).toCharArray();
+		for (char caracter : charArrayOfNumber) {
+			if (caracter == '3') {
+				sb.append("Foo");
+			}
+		}
+		if(sb.length() == 0) {
+			sb.append(number);
+		}
+		return sb.toString();
 	}
 
 }
