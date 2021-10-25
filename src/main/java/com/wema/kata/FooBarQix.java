e463fe1f2a697c5eea84406c35e3a55f943d252e
Hamda <hboufaied@wemanity.com>
2021-10-25 11:01:11 +0200

    should return BarBar when number is 5 : test ok

--- src/main/java/com/wema/kata/FooBarQix.java
+++ src/main/java/com/wema/kata/FooBarQix.java
@@ -3,24 +3,32 @@ package com.wema.kata;
 public class FooBarQix {
 
 	private static final String FOO = "Foo";
+	private static final String BAR = "Bar";
 
 	public String compute(int number) {
 		StringBuilder result = new StringBuilder();
 		if (number % 3 == 0) {
 			result.append(FOO);
 		}
+		if (number % 5 == 0) {
+			result.append(BAR);
+		}
 		char[] charArrayOfNumber = String.valueOf(number).toCharArray();
 		for (char caracter : charArrayOfNumber) {
 			if (caracter == '3') {
 				result.append(FOO);
 			}
+			if (caracter == '5') {
+				result.append(BAR);
+			}
 		}
-		
-		// if number is not devised by 3 or not contain 3, then we return the same number
+
+		// if number is not devised by 3 or not contain 3, then we return the same
+		// number
 		if (result.length() == 0) {
 			result.append(number);
 		}
-		
+
 		return result.toString();
 	}
 
